78f85208e004d66c571522638d880c53b0163070
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-06-10 17:53:18 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }]
+    }
+};
